7ed4515161a64d578795c11b046251212e8638c8
Santiago Ramos <sramos@oei.es>
2017-12-12 16:13:43 +0100

    Index

--- app/controllers/refuels_controller.rb
+++ app/controllers/refuels_controller.rb
@@ -4,6 +4,7 @@ class RefuelsController < ApplicationController
   # GET /refuels
   # GET /refuels.json
   def index
+    @refuels = Refuel.order("date desc")
   end
 
   # GET /refuels/1
--- app/models/refuel.rb
+++ app/models/refuel.rb
@@ -3,14 +3,38 @@ class Refuel < ApplicationRecord
   validates :odometer, :volume, numericality: { greater_than: 0.0 }
 
   validate :price_and_cost, if: :volume
+  validate :odometer_and_date
 
- private
+  def traveled
+    previous = previous_refuel
+    self.odometer - previous.odometer if previous
+  end
 
+  def consumption
+    trav = traveled 
+    return trav ? 100 * self.volume / (trav) : nil
+  end
+
+ #private
+
+  # price or cost calculation
   def price_and_cost
     if price
       self.cost = price * volume
     elsif cost
       self.price = cost / volume
     end
+  end
+
+  # Check odometer and date consistency
+  def odometer_and_date
+    previous = previous_refuel
+    errors.add :base, "Verifique la fecha" if previous && previous.date > date
+    return errors.empty?
+  end
+
+  # Return previous refuel
+  def previous_refuel
+    Refuel.where("odometer < ?", self.odometer).order(:odometer).last
   end
 end
