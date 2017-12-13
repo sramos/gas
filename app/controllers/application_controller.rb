b0dc8bcb23f8762d3fef3d565da76287dd0e2946
Santiago Ramos <sramos@oei.es>
2017-12-13 16:12:46 +0100

    Registros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :user_permitted_parameters, if: :devise_controller?
+
+ private
+
+  def user_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
